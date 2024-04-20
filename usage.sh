2890aad45bd9824de99c233c4e2413df266544d7
Jens Munk Hansen <jens.munk.hansen@gmail.com>
2024-04-20 16:56:49 +0200

    fixed path

--- usage.sh
+++ usage.sh
@@ -2,5 +2,6 @@
 
 rm -Rf $(pwd)/build/usage
 
-cmake -Bbuild/usage -H./Usage -DCMAKE_PREFIX_PATH=$(pwd)/install/lib/cmake
+cmake -Bbuild/usage -H./Usage -DCMAKE_FIND_ROOT_PATH=$(pwd)/install
 cmake --build build/usage --parallel 8
+
