4e701cd8727679dec49841ee511b22e7b8e924bc
Jens Munk Hansen <jens.munk.hansen@gmail.com>
2024-04-22 21:28:54 +0200

    copy DLLs also

--- install.bat
+++ install.bat
@@ -3,8 +3,16 @@ rmdir /Q /S install
 rmdir /Q /S build\library
 
 
-cmake -B build/library -H%~dp0/Library -DCMAKE_INSTALL_PREFIX=%~dp0/install 
-cmake --build build/library  --config Release --parallel 8
+set myArg=%1
 
-REM cmake -Bbuild/library -HLibrary -DCMAKE_INSTALL_PREFIX=$(pwd)/install -DCMAKE_BUILD_TYPE=Release
-REM cmake --build build/library --parallel 8 --target install
+REM Both shared and static are build
+if %myArg% == shared (
+  echo SHARED
+  cmake -B build/library -H%~dp0/Library -DCMAKE_INSTALL_PREFIX=%~dp0/install
+) else {
+  cmake -B build/library -H%~dp0/Library -DCMAKE_INSTALL_PREFIX=%~dp0/install
+)
+
+cmake --build build/library  --config Release --parallel 20 -- -maxCpuCount:20
+REM cmake --build build/library  --config Debug --parallel 20 -- -maxCpuCount:20
+
--- usage.bat
+++ usage.bat
@@ -1,5 +1,17 @@
 @echo off
 rmdir /Q /S build\usage
 
-cmake -Bbuild/usage -H./Usage -DCMAKE_PREFIX_PATH="%~dp0install"
-cmake --build build/usage --parallel 8
+set myArg=%1
+
+if %myArg% == shared (
+  echo SHARED
+  cmake -Bbuild/usage -H./Usage -DCMAKE_PREFIX_PATH="%~dp0install" -DBUILD_SHARED_LIBS=ON
+) else (
+  cmake -Bbuild/usage -H./Usage -DCMAKE_PREFIX_PATH="%~dp0install"
+)
+
+cmake --build build/usage --config Release --parallel 20 -- -maxCpuCount:20
+REM cmake --build build/usage --config Debug --parallel 20 -- -maxCpuCount:20
+
+build\usage\Release\main.exe
+REM build\usage\Debug\main.exe
