0f0d905a66dc40591f52b6519153cf70b5928a4f
Jens Munk Hansen <jens.munk.hansen@gmail.com>
2024-04-22 21:51:15 +0200

    ..

--- install.bat
+++ install.bat
@@ -2,6 +2,7 @@
 rmdir /Q /S install
 rmdir /Q /S build\library
 
+if [%1]==[] goto usage
 
 set myArg=%1
 
@@ -9,10 +10,13 @@ REM Both shared and static are build
 if %myArg% == shared (
   echo SHARED
   cmake -B build/library -H%~dp0/Library -DCMAKE_INSTALL_PREFIX=%~dp0/install
-) else {
+) else (
+  echo STATIC
   cmake -B build/library -H%~dp0/Library -DCMAKE_INSTALL_PREFIX=%~dp0/install
 )
 
 cmake --build build/library  --config Release --parallel 20 -- -maxCpuCount:20
 REM cmake --build build/library  --config Debug --parallel 20 -- -maxCpuCount:20
 
+:usage
+@echo Usage: %0 static/shared
--- usage.bat
+++ usage.bat
@@ -1,17 +1,23 @@
 @echo off
 rmdir /Q /S build\usage
 
+if [%1]==[] goto usage
+
 set myArg=%1
 
 if %myArg% == shared (
   echo SHARED
   cmake -Bbuild/usage -H./Usage -DCMAKE_PREFIX_PATH="%~dp0install" -DBUILD_SHARED_LIBS=ON
 ) else (
+  echo STATIC
   cmake -Bbuild/usage -H./Usage -DCMAKE_PREFIX_PATH="%~dp0install"
 )
 
 cmake --build build/usage --config Release --parallel 20 -- -maxCpuCount:20
-REM cmake --build build/usage --config Debug --parallel 20 -- -maxCpuCount:20
+cmake --build build/usage --config Debug --parallel 20 -- -maxCpuCount:20
 
 build\usage\Release\main.exe
-REM build\usage\Debug\main.exe
+build\usage\Debug\main.exe
+
+:usage
+@echo Usage: %0 static/shared
