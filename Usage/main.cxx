c67e76ce54a56f4f94d6513eebac9cdc87f5db22
Jens Munk Hansen <jens.hansen@shape.com>
2024-04-23 07:50:02 +0200

    Boost linking zlib statically

--- Usage/main.cxx
+++ Usage/main.cxx
@@ -1,8 +1,32 @@
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <boost/iostreams/filtering_streambuf.hpp>
+#include <boost/iostreams/copy.hpp>
+#include <boost/iostreams/filter/zlib.hpp>
 #include <zlib.h>
-#include <cstdio>
 
 int main(int argc, char *argv[])
 {
-  printf("%s\n", zlibVersion());
+
+  std::cout << zlibVersion() << std::endl;
+  
+  {
+    std::ofstream file("a.z", std::ios_base::out | std::ios_base::binary);
+    boost::iostreams::filtering_streambuf<boost::iostreams::output> out;
+    out.push(boost::iostreams::zlib_compressor());
+    out.push(file);
+    std::stringstream sstr{"nihao"};
+    boost::iostreams::copy(sstr, out);
+  }
+
+  {
+    std::ifstream file("a.z", std::ios_base::in | std::ios_base::binary);
+    boost::iostreams::filtering_streambuf<boost::iostreams::input> in;
+    in.push(boost::iostreams::zlib_decompressor());
+    in.push(file);
+  }
+
+  
   return 0;
 }
